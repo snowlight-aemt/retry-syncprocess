a3df67752371563fe9790675ce985b28ad6571e9
snowlight-aemt <it.s.aemt@gmail.com>
2023-06-09 08:58:42 +0900

    진행 1

--- src/test/java/me/snowlight/domain/ScheduleTest.java
+++ src/test/java/me/snowlight/domain/ScheduleTest.java
@@ -1,25 +1,29 @@
 package me.snowlight.domain;
 
 import me.snowlight.domain.team.TeamDao;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ScheduleTest {
     @Test
-    void name() {
-        MemoryRetryQueue mockRetryQueue = new MemoryRetryQueue();
-        mockRetryQueue.enQueue(new RetryDate(new TeamDao(1L, "team1", 2)), 1);
+    void run() {
+        MemoryRetryQueue retryQueue = new MemoryRetryQueue();
+        retryQueue.enQueue(new RetryDate(new TeamDao(1L, "team1", 2)), 1);
 
-        mockRetryQueue.enQueue(new RetryDate(new TeamDao(2L, "team1", 3)), 2);
-        mockRetryQueue.enQueue(new RetryDate(new TeamDao(3L, "team1", 4)), 2);
+        retryQueue.enQueue(new RetryDate(new TeamDao(2L, "team1", 3)), 2);
+        retryQueue.enQueue(new RetryDate(new TeamDao(3L, "team1", 4)), 2);
 
-        mockRetryQueue.enQueue(new RetryDate(new TeamDao(4L, "team1", 5)), 3);
-        mockRetryQueue.enQueue(new RetryDate(new TeamDao(5L, "team1", 6)), 3);
-        mockRetryQueue.enQueue(new RetryDate(new TeamDao(6L, "team1", 7)), 3);
+        retryQueue.enQueue(new RetryDate(new TeamDao(4L, "team1", 5)), 3);
+        retryQueue.enQueue(new RetryDate(new TeamDao(5L, "team1", 6)), 3);
+        retryQueue.enQueue(new RetryDate(new TeamDao(6L, "team1", 7)), 3);
 
-        Schedule schedule = new Schedule(new SyncRetry(new Sync(), mockRetryQueue));
+        Schedule schedule = new Schedule(new SyncRetry(new Sync(), retryQueue));
 
-        schedule.run(20000, 40000, 60000);
+        schedule.run(5000, 10000, 15000);
 
+        Assertions.assertThat(retryQueue.deQueAll(1)).hasSize(0);
+        Assertions.assertThat(retryQueue.deQueAll(2)).hasSize(0);
+        Assertions.assertThat(retryQueue.deQueAll(3)).hasSize(0);
     }
 
     private class Schedule {
