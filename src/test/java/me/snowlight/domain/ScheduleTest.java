c9a6d157785fd7cd1bd75c25034b32089bbdc902
snowlight-aemt <it.s.aemt@gmail.com>
2023-06-12 08:37:39 +0900

    통합 테스트 작성

--- src/test/java/me/snowlight/domain/ScheduleTest.java
+++ src/test/java/me/snowlight/domain/ScheduleTest.java
@@ -26,24 +26,4 @@ public class ScheduleTest {
         Assertions.assertThat(retryQueue.deQueAll(3)).hasSize(0);
     }
 
-    private class Schedule {
-        private final SyncRetry syncRetry;
-
-        public Schedule(SyncRetry syncRetry) {
-            this.syncRetry = syncRetry;
-        }
-
-        public void run(int ...repeat) {
-            int idx = 1;
-            for (int time : repeat) {
-                try {
-                    Thread.sleep(time);
-                    System.out.println(idx + "  ");
-                    this.syncRetry.retrySync(idx++);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-    }
 }
