18427259a84a49d90e0a01e6003c74c719cf2b30
snowlight-aemt <it.s.aemt@gmail.com>
2023-06-14 21:53:57 +0900

    FileRetryQueue 테스트 코드 구현

--- src/test/java/me/snowlight/domain/FileRetryQueueTest.java
+++ src/test/java/me/snowlight/domain/FileRetryQueueTest.java
@@ -2,15 +2,17 @@ package me.snowlight.domain;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.snowlight.domain.team.TeamDao;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class FileRetryQueueTest {
@@ -18,6 +20,13 @@ public class FileRetryQueueTest {
 
     public FileRetryQueueTest() {
         mapper = new ObjectMapper();
+    }
+
+    @AfterEach
+    public void post() throws Exception {
+        Files.deleteIfExists(Paths.get("build/retry-first-files"));
+        Files.deleteIfExists(Paths.get("build/retry-second-files"));
+        Files.deleteIfExists(Paths.get("build/retry-third-files"));
     }
 
     @Test
@@ -69,10 +78,71 @@ public class FileRetryQueueTest {
         }).isInstanceOf(RuntimeException.class);
     }
 
+    @Test
+    void deQueAll__read_retry_first_file() throws Exception {
+        RetryDate retryDate = new RetryDate(new TeamDao(1L, "Test A", 123));
+        Files.writeString(Paths.get("build/retry-first-files"), mapper.writeValueAsString(retryDate));
+
+        FileRetryQueue fileRetryQueue = new FileRetryQueue();
+        List<RetryDate> retryDates = fileRetryQueue.deQueAll(1);
+
+        assertThat(retryDates).hasSize(1);
+        assertThat(retryDates.get(0).getId()).isEqualTo(1L);
+        assertThat(retryDates.get(0).getName()).isEqualTo("Test A");
+        assertThat(retryDates.get(0).getMemberCount()).isEqualTo(123);
+    }
+
+    @Test
+    void deQueAll__read_retry_second_file() throws Exception {
+        RetryDate retryDate = new RetryDate(new TeamDao(1L, "Test A", 123));
+        Files.writeString(Paths.get("build/retry-second-files"), mapper.writeValueAsString(retryDate));
+
+        FileRetryQueue fileRetryQueue = new FileRetryQueue();
+        List<RetryDate> retryDates = fileRetryQueue.deQueAll(2);
+
+        assertThat(retryDates).hasSize(1);
+        assertThat(retryDates.get(0).getId()).isEqualTo(1L);
+        assertThat(retryDates.get(0).getName()).isEqualTo("Test A");
+        assertThat(retryDates.get(0).getMemberCount()).isEqualTo(123);
+    }
+
+    @Test
+    void deQueAll__read_retry_third_file() throws Exception {
+        RetryDate retryDate = new RetryDate(new TeamDao(1L, "Test A", 123));
+        Files.writeString(Paths.get("build/retry-third-files"), mapper.writeValueAsString(retryDate));
+
+        FileRetryQueue fileRetryQueue = new FileRetryQueue();
+        List<RetryDate> retryDates = fileRetryQueue.deQueAll(3);
+
+        assertThat(retryDates).hasSize(1);
+        assertThat(retryDates.get(0).getId()).isEqualTo(1L);
+        assertThat(retryDates.get(0).getName()).isEqualTo("Test A");
+        assertThat(retryDates.get(0).getMemberCount()).isEqualTo(123);
+    }
+
+    @Test
+    void deQueAll__nth_then_great_third() throws Exception {
+        RetryDate retryDate = new RetryDate(new TeamDao(1L, "Test A", 123));
+        Files.writeString(Paths.get("build/retry-third-files"), mapper.writeValueAsString(retryDate));
+
+        FileRetryQueue fileRetryQueue = new FileRetryQueue();
+        assertThatThrownBy(
+                () -> {
+                    fileRetryQueue.deQueAll(4);
+                }).isInstanceOf(RuntimeException.class);
+    }
+
     private class FileRetryQueue implements RetryQueue {
-        public static final String RETRY_FIRST_FILES = "build/retry-first-files";
-        public static final String RETRY_SECOND_FILES = "build/retry-second-files";
-        public static final String RETRY_THIRD_FILES = "build/retry-third-files";
+        public static final String RETRY_FIRST_PATH = "build/retry-first-files";
+        public static final String RETRY_SECOND_PATH = "build/retry-second-files";
+        public static final String RETRY_THIRD_PATH = "build/retry-third-files";
+        private static final String EMPTY_PATH = "";
+
+        private final ObjectMapper mapper;
+
+        public FileRetryQueue() {
+            mapper = new ObjectMapper();
+        }
 
         @Override
         public void enQueue(RetryDate retryDate, int nth) {
@@ -80,18 +150,8 @@ public class FileRetryQueueTest {
                 throw new RuntimeException();
             }
 
-            ObjectMapper mapper = new ObjectMapper();
-
-            String retryFiles = "";
-            if (nth == 1)
-                retryFiles = RETRY_FIRST_FILES;
-            else if (nth == 2)
-                retryFiles = RETRY_SECOND_FILES;
-            else if (nth == 3)
-                retryFiles = RETRY_THIRD_FILES;
-
             try {
-                Files.writeString(Paths.get(retryFiles), mapper.writeValueAsString(retryDate));
+                Files.writeString(getBackUpPath(nth), mapper.writeValueAsString(retryDate));
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -99,7 +159,34 @@ public class FileRetryQueueTest {
 
         @Override
         public List<RetryDate> deQueAll(int nth) {
-            return null;
+            if (nth > 3) {
+                throw new RuntimeException();
+            }
+
+            try {
+                List<String> lines = Files.readAllLines(getBackUpPath(nth));
+                List<RetryDate> retryDates = new ArrayList<>();
+
+                for (String str : lines) {
+                    retryDates.add(this.mapper.readValue(str, RetryDate.class));
+                }
+
+                return retryDates;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private Path getBackUpPath(int nth) {
+            if (nth == 1) {
+                return Paths.get(RETRY_FIRST_PATH);
+            } else if (nth == 2) {
+                return Paths.get(RETRY_SECOND_PATH);
+            } else if (nth == 3) {
+                return Paths.get(RETRY_THIRD_PATH);
+            }
+
+            return Paths.get(EMPTY_PATH);
         }
     }
 }
